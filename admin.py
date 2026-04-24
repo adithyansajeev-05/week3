d55d418afdf3a831dfb57de35046e33a259154e3
adithyansajeev-05 <adithyansajeev05@gmail.com>
2026-04-24 11:20:25 +0530

    added files

--- admin.py
+++ admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import Course
 
 admin.site.register(Course)
+<<<<<<< HEAD
 
 # Register your models here.
+=======
+# Register your models here.
+class CourseAdmin(admin.ModelAdmin):
+    list_display=('Course_name','price','status')
+>>>>>>> 7f36e1d (added files)
